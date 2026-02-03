39e1adc189facb86fa041cc77ff373032e2105f7
Mahdi Hasan <mehedi49891@gmail.com>
2026-02-03 20:02:59 +0600

    Use SQLite on Render

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'mamar_bank.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mamar_bank',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
